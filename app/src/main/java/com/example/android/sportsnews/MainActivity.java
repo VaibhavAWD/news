faaa6a3394d9d7fbff5e7e2625a527ee39e38898
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2017-08-18 07:21:45 +0530

    Implement loader callbacks and its methods

--- app/src/main/java/com/example/android/sportsnews/MainActivity.java
+++ app/src/main/java/com/example/android/sportsnews/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.sportsnews;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.sportsnews.Utils.News;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> newsList) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+
     }
 }
