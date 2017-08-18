16f0b6ba3febfc5283ba7a14547a3b52e75a9d5e
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2017-08-18 07:37:34 +0530

    Add object references and initialize them

--- app/src/main/java/com/example/android/sportsnews/MainActivity.java
+++ app/src/main/java/com/example/android/sportsnews/MainActivity.java
@@ -4,17 +4,41 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.android.sportsnews.Utils.News;
+import com.example.android.sportsnews.Utils.NewsAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
+
+    private RecyclerView mNewsList;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+
+    private View mEmptyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewsList = (RecyclerView) findViewById(R.id.news_list);
+
+        mNewsList.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mNewsList.setLayoutManager(mLayoutManager);
+
+        mAdapter = new NewsAdapter(new ArrayList<News>());
+        mNewsList.setAdapter(mAdapter);
+
+        mEmptyView = findViewById(R.id.empty_view);
+
     }
 
     @Override
